3cbb4832c94e37b933d088d7e417db9f4c85d9b7
andrewmd5 <1297077+andrewmd5@users.noreply.github.com>
2025-11-06 13:58:44 +0900

    fix: missing dir

--- hosts/dotnet/scripts/build-engine.sh
+++ hosts/dotnet/scripts/build-engine.sh
@@ -6,6 +6,8 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 ENGINE_DIR="${SCRIPT_DIR}/../../../engine"
 OUTPUT_DIR="${SCRIPT_DIR}/../Hako/Resources"
 
+mkdir -p "${OUTPUT_DIR}"
+
 cd "${ENGINE_DIR}"
 
 ./release-hako.sh "${OUTPUT_DIR}"
